4254e8de7bd2d9a0695e282c3b193b56ec8aaaff
Ani Valle <anilisethvalle@gmail.com>
2023-04-13 16:29:00 +0200

    + add progress bar

--- client/js/components/snpRequest.js
+++ client/js/components/snpRequest.js
@@ -13,5 +13,27 @@ $().ready(() => {
         $("#snp_step1").show();
         $("#snp_step2").hide();
     });
-});
 
+    // Show progress bar div
+    $("#btn_next_step2").on("click", () => { 
+        $("#div_progress").show();
+    });
+
+    // Hide the progress bar div
+    $("#btn_progress").on("click", () => {
+        $("#div_progress").hide();
+    });
+
+    // Ejemplo de actualizar el progress bar
+    const progressBar = document.getElementById('progress-bar');
+    const progressValue = document.getElementById('progress-value');
+
+    function actualizarBarraDeProgreso(valor) {
+        progressBar.value = valor;
+        progressValue.textContent = valor;
+    }
+    // Ejemplo de uso
+    actualizarBarraDeProgreso(50);
+
+      
+});
