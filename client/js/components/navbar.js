a2675ade6adc9aa9d5e2021f8cb6937fcf614f9c
MrGri00 <marcgarciat2000@gmail.com>
2023-04-19 18:06:07 +0200

    + Created "My Profile" button in navbar ~ Updated navbar.js to show/hide new button

--- client/js/components/navbar.js
+++ client/js/components/navbar.js
@@ -4,10 +4,12 @@ $().ready(() => {
         if (d.status_code === 200) {
             $("#btn-register").hide();
             $("#btn-login").hide();
+            $("#btn-myProfile").show();
             $("#btn-logout").show();
         } else {
             $("#btn-register").show();
             $("#btn-login").show();
+            $("#btn-myProfile").hide();
             $("#btn-logout").hide();
         }
     });
