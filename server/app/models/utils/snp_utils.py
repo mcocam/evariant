6450ed4b7996e196c6b15602535ff672b34ce6e8
marccocamoreno <marccocamoreno@gmail.com>
2023-04-24 20:06:54 +0200

    + MySQL: incorrect single fasta

--- server/app/models/utils/snp_utils.py
+++ server/app/models/utils/snp_utils.py
@@ -2,4 +2,8 @@ from Bio import Entrez
 from models.SingleFasta import SingleFasta
 
 def get_reference_genome(single_fasta: SingleFasta) -> str:
+    
+    query_term: str = f""
+    
+    
     pass
