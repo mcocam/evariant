dc1873c8a5233cf515b761bbb1ffdc4b2dbe8c42
marccocamoreno <marccocamoreno@gmail.com>
2023-04-24 19:22:56 +0200

    + SingleFasta class model: added sequence var

--- server/app/models/utils/snp_utils.py
+++ server/app/models/utils/snp_utils.py
@@ -1,9 +1,5 @@
 from Bio import Entrez 
 from models.SingleFasta import SingleFasta
 
-def get_reference_genome(genome_assembly: str, 
-                         chromosome: str,
-                         strand: int, 
-                         start: int,
-                         end: int) -> str:
+def get_reference_genome(single_fasta: SingleFasta) -> str:
     pass
