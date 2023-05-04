c9b5d39788eb0726dc4f7624a27ce34ad5e9499d
marccocamoreno <marccocamoreno@gmail.com>
2023-05-04 18:59:05 +0200

    - route_guard: remove alert

--- client/js/route_guard.js
+++ client/js/route_guard.js
@@ -6,7 +6,6 @@ $().ready(() => {
             console.log("Guard: Session ok!");
         } else {
             console.log("Guard: Session down!");
-            alert("You must be logged in to view this page!");
             window.location.href = './index.html';
         }
     });
