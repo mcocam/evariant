8e0c6e07ff02b32e07f68b5f62bc51115ba0519d
Ani Valle <anilisethvalle@gmail.com>
2023-05-10 19:54:01 +0200

    ~ In Process show phylogenetic tree

--- client/js/components/phylo_results.js
+++ client/js/components/phylo_results.js
@@ -1,19 +1,26 @@
+
 $().ready(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const fasta_id = urlParams.get('fasta_id');
+    const phyloResults = document.getElementById('see_results');
 
     axios.post('/api/phylo/results/' + fasta_id)
         .then(function (response) {
             xml = response.data.data.xml
-            console.log(response.data.data.xml);
-            
-            
+
+            draw = drawTree(xml);
             // const phylo = "Aquí se mostraran los results";
-            // const phyloResults = document.getElementById('see_results');
-            // $(phyloResults).text(phylo);
+            phyloResults.appendChild(draw);
 
         })
         .catch(function (error) {
-            console.log("Phylo catch " + error);
+            console.log("Phylo catch error: " + error);
         });
 })
+
+const drawTree = (xml) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(xml, "text/xml");
+    const svg = build(makeCompatTable(phyloxml.parse(doc)));
+    return svg.node();
+};
