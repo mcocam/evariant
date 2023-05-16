ca4238a3f063dbf8c0f2e8862b8566e839bfd117
MrGri00 <marcgarciat2000@gmail.com>
2023-05-16 17:26:21 +0200

    ~ Documented FastaDao.py

--- server/app/models/persist/FastaDao.py
+++ server/app/models/persist/FastaDao.py
@@ -26,7 +26,18 @@ class FastaDao:
         self.fasta_table:   Table  = fasta_table
         self.response: dict[str,any] = {"error": True, "message": "", "data": Fasta}
 
+    #----------------------------------------------------------------
     async def get_fasta_by_id(self, id: int) -> list[str] :
+        """Returns a fasta identified by the given id
+
+        Args:
+            id (int): The desired fasta's id
+
+        Returns:
+            list[str]:  If found, returns the parsed fasta, a message, and a boolean indicating that there was no error.
+                        If not found, simply returns a message saying so.
+                        If there was an exception, returns that as the message.
+        """
 
         response = self.response
 
@@ -51,11 +62,19 @@ class FastaDao:
         return response
     
 
-    # ADD FASTA 
+    #----------------------------------------------------------------
     def add_new_fasta(self,fasta: Fasta) -> int:
         """  Add a new Fasta to database
         Enters -> Object Fasta
         return -> bool
+        """
+        """Add a new Fasta to database
+
+        Args:
+            fasta (Fasta): The new fasta to add to the database
+
+        Returns:
+            int: The id assigned to the newly added fasta
         """
 
         new_fasta_added: int = 0
@@ -78,13 +97,18 @@ class FastaDao:
 
         return new_fasta_added
     
-
-    # Get fastas table information by user id and fasta type
-    # @param user_id:int
-    # @param type:int
-    # @return info_fasta: list[list]
     #----------------------------------------------------------------
     async def get_info(self, user_id: int, type_fasta) -> list[list]:
+        """Get fastas table information by user id and fasta type
+
+        Args:
+            user_id (int): The owner's id
+            type_fasta (bool): The type of fasta (0 = multifasta, 1 = singlefasta)
+
+        Returns:
+            list[list]: A list of all fastas belonging to the specified user.
+                        Each fasta is returned as a list, separated by the columns of the database
+        """
 
         info_fasta: list[list] = []
         
@@ -104,8 +128,16 @@ class FastaDao:
 
         return info_fasta    
 
-    #
+    #----------------------------------------------------------------
     def __parse_fasta(self,raw_data: list[any] ) -> Fasta:
+        """Reads a list of the components of a fasta, and parses it automatically into a Fasta object
+
+        Args:
+            raw_data (list[any]): A list of the fasta's components
+
+        Returns:
+            Fasta: The same information converted into a Fasta object
+        """
 
         fasta_id:       int         = raw_data[0]
         title:          str         = raw_data[1]
@@ -124,18 +156,18 @@ class FastaDao:
         
         return fasta
 
-# DELETE FASTA 
+    #----------------------------------------------------------------
     def delete_fasta(self, fasta_id: int) -> int:
-        """  Add a new Fasta to database
-        Enters -> Object Fasta
-        return -> bool
+        """Remove a fasta from the database
+
+        Args:
+            fasta_id (int): The id of the fasta to delete
+
+        Returns:
+            int: A code from the list indicating if the operation was successful, or what failed exactly
         """
 
         fasta_deleted: int = 0
-        # query = self.fasta_table.delete().values(
-        #     id = fasta_id,
-        #     user_id = fasta.user_id
-        # )
 
         query = self.fasta_table.delete().where(
             self.fasta_table.c.id == fasta_id
@@ -148,10 +180,10 @@ class FastaDao:
 
             print(dir(response))
 
-            if response.rowcount > 0:
-                fasta_deleted = response.deleted_primary_key[0]
+            if (response.rowcount <= 0):
+                fasta_deleted = 900
 
         except Exception as e:
-            print(e)
+            fasta_deleted = e
 
         return fasta_deleted
