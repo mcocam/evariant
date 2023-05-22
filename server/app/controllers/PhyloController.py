52e9fa127baa7bbadcdd8a944b88b0dd209ed6c2
marccocamoreno <marccocamoreno@gmail.com>
2023-05-22 20:26:28 +0200

    + Thread stop system

--- server/app/controllers/PhyloController.py
+++ server/app/controllers/PhyloController.py
@@ -3,7 +3,6 @@ from models.persist.FastaDao import FastaDao
 from models.persist.MultiFastaDao import MultiFastaDao
 from models.PhyloTree import PhyloTree
 from models.Fasta import Fasta
-from tempfile import TemporaryDirectory
 from pathlib import Path
 
 from Bio import AlignIO
@@ -14,10 +13,15 @@ from Bio.Phylo.PhyloXML import Phylogeny
 import re
 
 import subprocess
-#from threading import Thread, Event
 from multiprocessing import Process
+import os
+import signal
+from uuid import uuid4
+from shutil import rmtree
 
 active_threads: dict[Process] = {}
+active_subprocess: dict[int] = {}
+temporal_folders: dict[Path] = {}
 
 class PhyloController:
     
@@ -48,10 +52,12 @@ class PhyloController:
         try:
             thread = Process(target=PhyloController.do_clustalo_alignment, args=(fasta.get_id(),fasta.get_raw_fasta(), fasta.get_user_id()))
             thread.start()
+            
             active_threads[fasta.get_id()] = thread
             
             if thread.is_alive():
                 is_processing = True 
+            
         except Exception as e:
             self.multi_fasta_dao.delete_multi(fasta.get_id())
             self.fasta_dao.delete_fasta(fasta.get_id())
@@ -92,85 +98,111 @@ class PhyloController:
 
     async def del_phylo(self, fasta_id) -> int:
 
-            try:
-                phylo_deleted = self.dao.delete_phylo(fasta_id)
+        try:
+            phylo_deleted = self.dao.delete_phylo(fasta_id)
+            
+            if fasta_id in active_threads:
                 
-                if fasta_id in active_threads:
-                    thread = active_threads[fasta_id]
-                    thread.terminate()
-                    print(f"Thread stopped")
-                    
+                thread = active_threads[fasta_id]
+                thread.terminate()
+                thread.join()
                 
-            except Exception as e:
-                print(f"Delete phylo Controller: {e}")
+                process = active_subprocess[fasta_id]
+                process.kill()
+                os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+                
+                temporal_path = temporal_folders[fasta_id]
+                rmtree(temporal_path)
+                
+                del active_threads[fasta_id]
+                del active_subprocess[fasta_id]
+                del temporal_folders[fasta_id]
+            
+        except Exception as e:
+            print(f"Delete phylo Controller: {e}")
 
-            return phylo_deleted
+        return phylo_deleted
         
     def do_clustalo_alignment(fasta_id: int, fasta_seq: str, fasta_user_id: int):
+        
+        temp_dir = Path(f"/temporal")
+        temp_dir.mkdir(exist_ok=True)
+        
+        temporal_path = temp_dir/str(uuid4())
+        temporal_path.mkdir()
         
         
         try:
             fasta_content = fasta_seq
-        
-            with TemporaryDirectory() as temp_dir:
-        
-                temp_dir_path: Path = Path(temp_dir)
-                
-                fasta_file_path = temp_dir_path/"fasta.fasta"
-                phylo_file_path = temp_dir_path/"phylo.nwk"
-                
-                with open(fasta_file_path, "w") as fasta_file:
-                    fasta_file.write(fasta_content)
-                    
-                clustalo_cline = ClustalOmegaCommandline(
-                                        infile = temp_dir_path/"fasta.fasta",
-                                        outfile = temp_dir_path/"output.fasta",
-                                        threads = 2,
-                                        seqtype = "DNA",
-                                        max_guidetree_iterations = 1,
-                                        max_hmm_iterations=100,
-                                        iterations=1
-                                        )
-                
-                cmd_string = str(clustalo_cline)
-
-                nice_cmd = f"nice -n 19 {cmd_string}"
-                    
-                subprocess.run(nice_cmd, shell=True)
-                    
-                alignment_file_path = list(temp_dir_path.glob("output.fasta"))
-                    
-                if len(alignment_file_path) == 1:
-                    alignment_file_path = alignment_file_path[0]
-                else:
-                    raise Exception("Incorrect alignment")
-                
-                clustal_alignment = AlignIO.read(alignment_file_path, "fasta")
             
-                calcultaor = DistanceCalculator("identity")
-                distance_matrix = calcultaor.get_distance(clustal_alignment)
-                
-                tree_constructor = DistanceTreeConstructor()
-                phylo_tree = tree_constructor.upgma(distance_matrix)
-                phylo_newick= Phylogeny.from_tree(phylo_tree)
-                
-                with open(phylo_file_path, "w") as phylo_file:
-                    Phylo.write(phylo_newick,phylo_file,"newick")
-                    
-                phylo_newick_str = ""
-                with open(phylo_file_path, "r") as newick:
-                    phylo_newick_str = newick.read() 
-                    
-                phylo = PhyloTree(fasta_id,
-                                fasta_user_id,
-                                phylo_newick_str)
-                
-                PhyloController().save_phylo(phylo)
-                
+            input_fasta_path = temporal_path/"fasta.fasta"
+            output_fasta_path = temporal_path/"output.fasta"
+            phylo_file_path = temporal_path/"phylo.nwk"
+            
+            with open(input_fasta_path, "w+") as input_file:
+                input_file.write(fasta_content)
                 
                     
+            clustalo_cline = ClustalOmegaCommandline(
+                                infile = input_fasta_path.absolute(),
+                                outfile = output_fasta_path.absolute(),
+                                threads = 2,
+                                seqtype = "DNA",
+                                max_guidetree_iterations = 1,
+                                max_hmm_iterations=100,
+                                iterations=1,
+                                verbose = True
+                                )
+        
+            cmd_string = str(clustalo_cline)
+            
+            nice_cmd = f"nice -n 19 {cmd_string}"
+                
+            process = subprocess.Popen(nice_cmd, 
+                                        shell=True, 
+                                        preexec_fn=os.setsid, 
+                                        stdin=subprocess.PIPE, 
+                                        stdout=subprocess.PIPE, 
+                                        stderr=subprocess.STDOUT, 
+                                        bufsize=1)
+            
+            temporal_folders[fasta_id] = temporal_path
+            active_subprocess[fasta_id] = process
+            process.communicate()
+                
+            alignment_file_path = list(temporal_path.glob("output.fasta"))
+                
+            if len(alignment_file_path) == 1:
+                alignment_file_path = alignment_file_path[0]
+            else:
+                raise Exception("Incorrect alignment")
+            
+            clustal_alignment = AlignIO.read(alignment_file_path, "fasta")
+        
+            calcultaor = DistanceCalculator("identity")
+            distance_matrix = calcultaor.get_distance(clustal_alignment)
+            
+            tree_constructor = DistanceTreeConstructor()
+            phylo_tree = tree_constructor.upgma(distance_matrix)
+            phylo_newick= Phylogeny.from_tree(phylo_tree)
+            
+            with open(phylo_file_path, "w") as phylo_file:
+                Phylo.write(phylo_newick,phylo_file,"newick")
+                
+            phylo_newick_str = ""
+            with open(phylo_file_path, "r") as newick:
+                phylo_newick_str = newick.read() 
+                
+            phylo = PhyloTree(fasta_id,
+                            fasta_user_id,
+                            phylo_newick_str)
+            
+            PhyloController().save_phylo(phylo)
+                
         except Exception as e:
             PhyloController().multi_fasta_dao.delete_multi(fasta_id)
             PhyloController().fasta_dao.delete_fasta(fasta_id)
             print(f"Error on ClustalO: {e}")
+        finally:
+            rmtree(temporal_path)
         
