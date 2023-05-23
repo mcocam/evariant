d33cc2aca5241508bb409de99ca2081dc4cff8e0
melaniaprado <mepr0206@office.proven.cat>
2023-05-23 13:13:57 +0200

    snp_request_results_files

--- client/js/components/snp_request_results.js
+++ client/js/components/snp_request_results.js
@@ -1,34 +1,152 @@
+/* SNP results request results page js
+@author: Melania Prado */
+
 $().ready(() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const fasta_id = urlParams.get('fasta_id');
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const fasta_id = urlParams.get('fasta_id');
+  axios.get('/api/snp/results/' + fasta_id)
+    .then(function (response) {
+      const snpRefs = response.data[0].snp_references;
+      const snpGenotypes = response.data[1].snp_genotypes;
 
-    axios.get('/api/snp/results/' + fasta_id)
-        .then(function (response) {
-            const snps = response.data;
-            
-            const resultsBody = document.getElementById('results_body');
+      const snpRelatedArticles = response.data[1].snp_articles;
+      const snpArticlesURL = response.data[1].snp_articles_url;
+      const articlesTitles = response.data[1].snp_articles_titles;
 
-            for (let i = 0; i < snps.length; i++) {
-                const snp = snps[i];
-                const row = document.createElement('tr');
-                const fastaId = document.createElement('td');
-                const refNucleotide = document.createElement('td');
-                const varNucleotide = document.createElement('td');
-                fastaId.innerText = snp.fasta_id;
-                refNucleotide.innerText = snp.ref_nucleotide;
-                varNucleotide.innerText = snp.var_nucleotide;
+      const snpRegions = response.data[1].snp_regions;
 
-                row.appendChild(fastaId);
-                row.appendChild(refNucleotide);
-                row.appendChild(varNucleotide);
+      const accordionContainer = $('#accordion');
 
-                resultsBody.appendChild(row);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-            console.log('catch');
-        });
+      // Iterate over each SNP key in the response
+      snpRefs.forEach(function (snpRef) {
+        const snpValues = snpGenotypes[snpRef];
 
+        // Create the accordion for the current SNP
+        const accordion = $('<div>').addClass('accordion').attr('id', `${snpRef}-accordion`);
+        accordionContainer.append(accordion);
+
+
+
+        // GENOTYPES ============================================================================================================================================================
+
+
+        //Genotype Comparative Card
+        const genotypeCard = $('<div>').addClass('card snp-result-card');
+        const genotypeCardHeader = $('<div>').addClass('card-header d-flex');
+        const genotypeCardContent = $('<div>').addClass('collapse show').attr('id', `${snpRef}-genotype-collapse`);
+        const genotypeCardBody = $('<div>').addClass('card-body');
+
+        // Add the SNP reference and genotype to the genotype card header
+        const snpRefText = $('<span>').addClass('snp-ref').text(snpRef);
+        const genotype = snpValues[0];
+        const genotypeText = $('<span>').addClass('me-3').text(`Genotype ${genotype}`);
+        genotypeCardHeader.append(snpRefText, genotypeText);
+
+        // Add the genotype card header and content to the genotype card
+        genotypeCard.append(genotypeCardHeader, genotypeCardContent);
+
+        // Add the genotype card to the accordion
+        accordion.append(genotypeCard);
+
+        // Genotype Comparative Table
+        const genotypeTable = $('<table>').addClass('table');
+        const genotypeTableHead = $('<thead>');
+        const genotypeTableBody = $('<tbody>');
+
+        // Create table header
+        const genotypeTableHeaderRow = $('<tr>');
+        const genotypeHeader = $('<th>').attr('scope', 'col').text('Genotype');
+        const magnitudeHeader = $('<th>').attr('scope', 'col').text('Magnitude');
+        const summaryHeader = $('<th>').attr('scope', 'col').text('Summary');
+        genotypeTableHeaderRow.append(genotypeHeader, magnitudeHeader, summaryHeader);
+        genotypeTableHead.append(genotypeTableHeaderRow);
+
+        // Create table rows for each SNP genotype
+        for (let i = 0; i < snpValues.length; i += 3) {
+          const snpGenotype = snpValues[i];
+          const magnitude = snpValues[i + 1];
+          const summary = snpValues[i + 2];
+
+          const tableRow = $('<tr>');
+          const genotypeCell = $('<td>').addClass('genotype').text(snpGenotype);
+          const magnitudeCell = $('<td>').text(magnitude);
+          const summaryCell = $('<td>').text(summary);
+          tableRow.append(genotypeCell, magnitudeCell, summaryCell);
+          genotypeTableBody.append(tableRow);
+        }
+
+        // Add the genotype table to the genotype card body
+        genotypeCardBody.append(genotypeTable.append(genotypeTableHead, genotypeTableBody));
+
+        // Add the genotype card body to the genotype card content
+        genotypeCardContent.append(genotypeCardBody);
+
+
+
+        // RELATED ARTICLES ====================================================================================================================================================
+        // Related Articles Card
+        const articlesCard = $('<div>').addClass('card snp-result-card');
+        const articlesCardHeader = $('<div>').addClass('card-header d-flex');
+        const articlesCardContent = $('<div>').addClass('collapse show').attr('id', `${snpRef}-articles-collapse`);
+        const articlesCardBody = $('<div>').addClass('card-body');
+
+        // Add the related articles card header
+        const articlesHeader = $('<h5>', { class: 'mb-0', text: 'Related Articles' });
+        articlesCardHeader.append(articlesHeader);
+
+        // Add the articles card header and content to the articles card
+        articlesCard.append(articlesCardHeader, articlesCardContent);
+
+        // Add the articles card to the accordion
+        accordion.append(articlesCard);
+
+        // Related Articles Table
+        const articlesTable = $('<table>', { class: 'table' });
+        const articlesTableHead = $('<thead>');
+        const articlesTableBody = $('<tbody>');
+
+        // Create table header
+        const articlesTableHeaderRow = $('<tr>');
+        const articlePMIDHeader = $('<th>').attr('scope', 'col').text('PMID');
+        const articleTitleHeader = $('<th>').attr('scope', 'col').text('Title');
+        articlesTableHeaderRow.append(articlePMIDHeader, articleTitleHeader);
+        articlesTableHead.append(articlesTableHeaderRow);
+
+
+        // Create table rows for each related article of the current SNP
+        const articleArray = snpRelatedArticles[snpRef];
+        const articleTitleText = articlesTitles[snpRef]; // Use snpRef as the key to access the title
+
+        if (articleArray && articleArray.length > 0) {
+          articleArray.forEach(function (article, index) {
+            const tableRow = $('<tr>');
+            const articlePMIDCell = $('<td>').addClass('article-PMID').text(article);
+            const articleTitleCell = $('<td>').addClass('article-title').text(articleTitleText[index]);
+            tableRow.append(articlePMIDCell, articleTitleCell);
+            articlesTableBody.append(tableRow);
+
+            // Add click event listener to open related article URL in a new tab
+            tableRow.on('click', function () {
+              window.open(snpArticlesURL[article], '_blank');
+            });
+          });
+        } else {
+          const noArticlesMessage = $('<tr>').append($('<td>').attr('colspan', '2').text('No related articles found.'));
+          articlesTableBody.append(noArticlesMessage);
+        }
+
+        // Add the articles table to the articles card body
+        articlesCardBody.append(articlesTable.append(articlesTableHead, articlesTableBody));
+
+        // Add the articles card body to the articles card content
+        articlesCardContent.append(articlesCardBody);
+
+      });
+
+      // Add event listener to collapse/expand accordion
+    })
+    .catch(function (error) {
+      console.log(error.stack);
+    });
 });
--- server/app/routes/snp/snp.py
+++ server/app/routes/snp/snp.py
@@ -1,4 +1,4 @@
-# Class Snp
+# SNP results router
 # @author: Melania Prado
 
 from fastapi import APIRouter, Response, Depends, HTTPException
@@ -7,6 +7,8 @@ from controllers.SnpController import SnpController
 from controllers.FastaController import FastaController
 from models.Snp import Snp
 from routes.users.session import cookie, SessionData, verifier
+from models.utils.scraper_utils import get_snpedia_pages, get_snp_data
+
 
 router: APIRouter = APIRouter(
     prefix="/snp",
@@ -25,7 +27,7 @@ async def get_snp_results(fasta_id: str, session_data: SessionData = Depends(ver
         session: A SessionData object with the current user session data.
 
     Returns:
-        A list with the SNPs for the given FASTA ID.
+        A dictionary with the SNPs for the given FASTA ID and their related data from SNPedia.
     """
 
     try:
@@ -37,17 +39,30 @@ async def get_snp_results(fasta_id: str, session_data: SessionData = Depends(ver
         request = request['data']
         fasta_user_id = request.get_user_id()
         print(fasta_user_id)
-        # Verify if the user has access to the 
-        if session_data.id != fasta_user_id:
-        #if session_data.id != request["user_id"]:
-            raise HTTPException(status_code=403, detail="User does not have access to the request.")
 
-        snps = await snp_controller.get_snps_by_fasta_id(fasta_id)
+        # Verify if the user has access to the request
+        if session_data.id != fasta_user_id:
+            raise HTTPException(status_code=403, detail="User does not have access to the request.")
         
-        return snps
+        snps = await snp_controller.get_snps_by_fasta_id(fasta_id)
+        #snp_refs = await snp_controller.get_snp_refs_by_fasta_id(fasta_id)
+        #print('snp_refs')
+        #print(snp_refs)
+
+        snp_refs = ['rs1815739', 'rs6152', 'rs1800497'] # For testing purposes
+        snpedia_pages = get_snpedia_pages(snp_refs)
+        snp_data = get_snp_data(snp_refs, snpedia_pages)
+
+        # Recopilar los resultados en un diccionario
+        snp_refs_dict = {
+            'snp_references': snp_refs
+        }
     
+        return snp_refs_dict, snp_data
+
     except HTTPException:
         raise
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
