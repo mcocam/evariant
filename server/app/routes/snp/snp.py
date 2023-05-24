6c368aaf7801c4897832b46bc49fe59715eee632
melaniaprado <mepr0206@office.proven.cat>
2023-05-24 15:41:44 +0200

    changes related to snp results display

--- server/app/routes/snp/snp.py
+++ server/app/routes/snp/snp.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from controllers.SnpController import SnpController
 from controllers.FastaController import FastaController
 from routes.users.session import cookie, SessionData, verifier
-from models.utils.scraper_utils import get_snpedia_pages, get_snp_data
+from models.utils.scraper_utils import get_snpedia_pages, get_snp_genotypes, get_snp_articles, get_articles_urls, get_snp_articles_titles
 
 
 router: APIRouter = APIRouter(
@@ -43,24 +43,46 @@ async def get_snp_results(fasta_id: str, session_data: SessionData = Depends(ver
             raise HTTPException(status_code=403, detail="User does not have access to the request.")
         
         snps = await snp_controller.get_snps_by_fasta_id(fasta_id)
-        #snp_refs = await snp_controller.get_snp_refs_by_fasta_id(fasta_id)
-        #print('snp_refs')
-        #print(snp_refs)
-
-        snp_refs = ['rs1815739', 'rs6152', 'rs1800497'] # For testing purposes
+        snp_ref_nucleotides = [snp.get_ref_nucleotide() for snp in snps]
+      
+        snp_var_nucleotides = [snp.get_var_nucleotide() for snp in snps]
+     
+        snp_refs = await snp_controller.get_snp_refs_by_fasta_id(fasta_id)
+        print('snp_refs')
+        print(snp_refs)
+        #snp_refs = ['rs1815739', 'rs6152', 'rs1800497'] # For testing purposes
         snpedia_pages = get_snpedia_pages(snp_refs)
-        snp_data = get_snp_data(snp_refs, snpedia_pages)
+        snp_genotypes = get_snp_genotypes(snp_refs, snpedia_pages)
+        snp_articles = get_snp_articles(snp_refs, snpedia_pages)
+        snp_articles_titles = get_snp_articles_titles(snp_refs, snpedia_pages)
+        snp_articles_url = get_articles_urls(snp_refs, snpedia_pages)
+
+
+        print('snp_ref_nucleotides')
+        print(snp_ref_nucleotides)
+
+        #return snp_genotypes, snp_articles, snp_articles_titles, snp_articles_url
+        #snp_data = get_snp_data(snp_refs, snpedia_pages)
 
         # Recopilar los resultados en un diccionario
-        snp_refs_dict = {
-            'snp_references': snp_refs
+        snp_data_dict = {
+            'snp_ref_nucleotides': snp_ref_nucleotides,
+            'snp_var_nucleotides': snp_var_nucleotides,
+            'snp_refs': snp_refs,
+            'snp_genotypes': snp_genotypes,
+            'snp_articles': snp_articles,
+            'snp_articles_titles': snp_articles_titles,
+            'snp_articles_url': snp_articles_url
         }
-    
-        return snp_refs_dict, snp_data
+        print('snp_data_dict')
+        print(snp_data_dict)
+        return snp_data_dict
 
     except HTTPException:
+        print(HTTPException)
         raise
     
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail=str(e))
     
